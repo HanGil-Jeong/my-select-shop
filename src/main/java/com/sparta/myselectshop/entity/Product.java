25df1684e861571785c9f3a14afe3dd8b315bb88
Hangil <jhg9240@gmail.com>
2026-06-25 17:23:22 +0900

    Scheduler 기능 구현

--- src/main/java/com/sparta/myselectshop/entity/Product.java
+++ src/main/java/com/sparta/myselectshop/entity/Product.java
@@ -2,6 +2,7 @@ package com.sparta.myselectshop.entity;
 
 import com.sparta.myselectshop.dto.ProductMyPriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.naver.dto.ItemDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -41,6 +42,10 @@ public class Product extends Timestamped {
 	}
 
 	public void update(ProductMyPriceRequestDto requestDto) {
-			this.myprice = requestDto.getMyprice();
+		this.myprice = requestDto.getMyprice();
+	}
+
+	public void updateByItemDto(ItemDto itemDto) {
+		this.myprice = itemDto.getLprice();
 	}
 }
